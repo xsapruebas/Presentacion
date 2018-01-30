64a636ce400ee563d29da77a2a8f69b65f9da2c0
xsapruebas <xsapruebas@gmail.com>
2018-01-30 12:54:03 +0100

    Se incluye el soporte para Firebase para cargar las imágenes

--- app/src/main/java/com/xsa/mobilityfriday/presentacion/Main2Activity.kt
+++ app/src/main/java/com/xsa/mobilityfriday/presentacion/Main2Activity.kt
@@ -2,12 +2,21 @@ package com.xsa.mobilityfriday.presentacion
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main2.*
+
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        ivAvatar.loadUrl(intent.getStringExtra("AVATAR"))
+        tvDescripcion.text = intent.getStringExtra("DESCRIPCION")
+    }
+    fun ImageView.loadUrl(url: String) {
+        Picasso.with(context).load(url).into(this)
     }
 }
--- app/src/main/java/com/xsa/mobilityfriday/presentacion/MainActivity.kt
+++ app/src/main/java/com/xsa/mobilityfriday/presentacion/MainActivity.kt
@@ -38,14 +38,14 @@ class MainActivity : AppCompatActivity() {
 
     fun getSuperheros(): MutableList<Superhero> {
         var superheros: MutableList<Superhero> = ArrayList()
-        superheros.add(Superhero("Spiderman", "Marvel", "Peter Parker", "https://cursokotlin.com/wp-content/uploads/2017/07/spiderman.jpg"))
-        superheros.add(Superhero("Daredevil", "Marvel", "Matthew Michael Murdock", "https://cursokotlin.com/wp-content/uploads/2017/07/daredevil.jpg"))
-        superheros.add(Superhero("Wolverine", "Marvel", "James Howlett", "https://cursokotlin.com/wp-content/uploads/2017/07/logan.jpeg"))
-        superheros.add(Superhero("Batman", "DC", "Bruce Wayne", "https://cursokotlin.com/wp-content/uploads/2017/07/batman.jpg"))
-        superheros.add(Superhero("Thor", "Marvel", "Thor Odinson", "https://cursokotlin.com/wp-content/uploads/2017/07/thor.jpg"))
-        superheros.add(Superhero("Flash", "DC", "Jay Garrick", "https://cursokotlin.com/wp-content/uploads/2017/07/flash.png"))
-        superheros.add(Superhero("Green Lantern", "DC", "Alan Scott", "https://cursokotlin.com/wp-content/uploads/2017/07/green-lantern.jpg"))
-        superheros.add(Superhero("Wonder Woman", "DC", "Princess Diana", "https://cursokotlin.com/wp-content/uploads/2017/07/wonder_woman.jpg"))
+        superheros.add(Superhero("Spiderman", "Marvel", "Peter Parker", "https://firebasestorage.googleapis.com/v0/b/notablenotes-28616.appspot.com/o/spiderman.jpg?alt=media&token=012b38fa-24a0-494c-8f59-7fe1c0f7dae7", "Gracias a su traje puede lanzar telarañas de las cuales se cuelga"))
+        superheros.add(Superhero("Daredevil", "Marvel", "Matthew Michael Murdock", "https://firebasestorage.googleapis.com/v0/b/notablenotes-28616.appspot.com/o/daredevil.jpg?alt=media&token=5994f4d1-8e59-44a4-a72c-9791e0e9c14c", "Está ciego pero puede ver más que el resto"))
+        superheros.add(Superhero("Wolverine", "Marvel", "James Howlett", "https://firebasestorage.googleapis.com/v0/b/notablenotes-28616.appspot.com/o/logan.jpeg?alt=media&token=eb3895ac-ecba-47ee-a24b-3b9f67d4c7bc", "Su esqueleto está formado de adamamtium, además se regenera"))
+        superheros.add(Superhero("Batman", "DC", "Bruce Wayne", "https://firebasestorage.googleapis.com/v0/b/notablenotes-28616.appspot.com/o/batman.jpg?alt=media&token=96a3c5c9-06d7-4362-8868-fc131ebb1528", "Mataron a sus padres y desde entonces busca venganza en Gotham"))
+        superheros.add(Superhero("Thor", "Marvel", "Thor Odinson", "https://firebasestorage.googleapis.com/v0/b/notablenotes-28616.appspot.com/o/thor.jpg?alt=media&token=b20dc66b-be40-4529-9a85-2dcc97c5c5c9", "Un dios con un martillo"))
+        superheros.add(Superhero("Flash", "DC", "Jay Garrick", "https://firebasestorage.googleapis.com/v0/b/notablenotes-28616.appspot.com/o/flash.png?alt=media&token=d58a61c1-8160-49c9-b3a9-a58437cbcf63", "Correo mucho, tanto que ni le ves"))
+        superheros.add(Superhero("Green Lantern", "DC", "Alan Scott", "https://firebasestorage.googleapis.com/v0/b/notablenotes-28616.appspot.com/o/green-lantern.jpg?alt=media&token=e154d850-3897-4350-b3a3-110657a66b70", "Tiene una linterna verde, pero no sé para que la necesita"))
+        superheros.add(Superhero("Wonder Woman", "DC", "Princess Diana", "https://firebasestorage.googleapis.com/v0/b/notablenotes-28616.appspot.com/o/wonder_woman.jpg?alt=media&token=300d9e5f-26bf-4ce7-986e-74d7fc66c4c8", "La única superheroína y tiene nombre de princesa real"))
         return superheros
     }
 }
@@ -54,7 +54,8 @@ data class Superhero(
         var superhero: String,
         var publisher: String,
         var realName: String,
-        var photo: String
+        var photo: String,
+        var description: String
 )
 
 class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
@@ -105,8 +106,10 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
         fun abrirTarjeta(context: Context, superhero: Superhero) {
             val intent = Intent(context, Main2Activity::class.java)
+            intent.putExtra("DESCRIPCION", superhero.description)
+            intent.putExtra("AVATAR", superhero.photo)
             startActivity(context, intent, null)
-            Toast.makeText(context, "CAMBIO DE ACTIVIDAD", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "DESCRIPCION DE: " + superhero.superhero, Toast.LENGTH_SHORT).show()
         }
     }
 }

3412e9a862d381eb3d1abad8f79cb72145ed299e
xsapruebas <xsapruebas@gmail.com>
2018-01-30 16:39:08 +0100

    Se incluye el soporte para Firebase para cargar las imágenes

---
+++ app/src/main/java/com/xsa/mobilityfriday/presentacion/AgregarActivity.kt
@@ -0,0 +1,12 @@
+package com.xsa.mobilityfriday.presentacion
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AgregarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.agregar)
+    }
+}
--- app/src/main/java/com/xsa/mobilityfriday/presentacion/MainActivity.kt
+++ app/src/main/java/com/xsa/mobilityfriday/presentacion/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+
 import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -26,6 +27,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setUpRecyclerView()
+
+        fab.setOnClickListener {
+            val intent = Intent(this, AgregarActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
     fun setUpRecyclerView() {
